feba5728776ad3733dd2f3199aead55cc702878d
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-08 20:40:06 +0530

    display featured projects section on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { FeaturedProjectsSection } from "@/components/home/featured-projects"
 import { HeroSection } from "@/components/home/hero"
 
 export default function Page() {
   return (
     <>
       <HeroSection />
+      <FeaturedProjectsSection />
     </>
   )
 }
